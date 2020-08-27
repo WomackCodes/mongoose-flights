a4e2dccf3224942de76f9ac205ce6ff44cb1b6f2
WomackCodes <chris@womack.codes>
2020-08-27 00:39:48 -0700

    CSS with Nick

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,6 +1,6 @@
 const Flight = require('../models/flight');
 const Ticket = require("../models/ticket");
-const { render } = require("../server");
+
 
 module.exports = {
     new: newTicket,
@@ -19,8 +19,9 @@ function create(req, res) {
     };
     const ticket = new Ticket(ticketObj);
     ticket.save(function (err) {
-        if (err)
-            res.render("tickets/new", { title: "Tickets", flightID: req.params.id });
+        if (err){
+        res.render("tickets/new", { title: "Tickets", flightID: req.params.id });
+        }
         res.redirect(`/flights/${req.params.id}`);
     });
 }
